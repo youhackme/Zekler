1d9844df3a901ea71b0edcb1c0b47cb08bb9bcfa
Hyder Bangash <hyder.bangash@stellatelecom.com>
2016-12-22 20:13:00 +0400

    Added gulp to workflow

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/style.scss')
+        .pipe(sass()) // Using gulp-sass
+        .pipe(gulp.dest('app/css/test.css'))
+});
