566bbc534e6fa9db144d44f80687a3e599575674
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-01-10 21:28:47 +0400

    Added bootstrap source in workflow

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,62 @@ gulp.task('sass', function () {
         .pipe(sass()) // Using gulp-sass
         .pipe(gulp.dest('app/css/'))
 });
+
+
+// source and distribution folder
+var source = 'app/',
+    dest = 'build/';
+
+// Bootstrap scss source
+var bootstrapSass = {
+    in: './node_modules/bootstrap-sass/'
+};
+
+// Bootstrap fonts source
+var fonts = {
+    in: [source + 'fonts/*.*', bootstrapSass.in + 'assets/fonts/**/*'],
+    out: dest + 'fonts/'
+};
+
+var img = {
+    in: [source + 'img/*.*',],
+    out: dest + 'img/'
+}
+
+// Our scss source folder: .scss files
+var scss = {
+    in: source + 'scss/main.scss',
+    out: dest + 'css/',
+    watch: source + 'scss/**/*',
+    sassOpts: {
+        outputStyle: 'nested',
+        precison: 3,
+        errLogToConsole: true,
+        includePaths: [bootstrapSass.in + 'assets/stylesheets']
+    }
+};
+
+// copy bootstrap required fonts to dest
+gulp.task('fonts', function () {
+    return gulp
+        .src(fonts.in)
+        .pipe(gulp.dest(fonts.out));
+});
+
+gulp.task('bootstrap', function () {
+    return gulp.src(scss.in)
+        .pipe(sass(scss.sassOpts))
+        .pipe(gulp.dest(scss.out));
+});
+
+
+gulp.task('img', function () {
+    return gulp.src(img.in)
+        .pipe(gulp.dest(img.out));
+});
+
+
+// default task
+gulp.task('default', ['sass'], function () {
+    gulp.watch(scss.watch, ['sass']);
+});
