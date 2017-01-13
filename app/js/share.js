90805a6c7e9ad76fc21edd5e996d01d397bd0658
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-01-14 02:12:38 +0400

    Added floating bar

--- app/js/share.js
+++ app/js/share.js
@@ -1,3 +1,57 @@
-/**
- * Created by Hyder on 14/01/2017.
- */
+var shareBlock = {
+    config: {
+        offset: 120,//px
+        appearWhenScrolledTo: {//%
+            start: 5,
+            stop: 93
+        }
+    },
+    init: function (config) {
+        $.extend(this.config, config);
+        this.hide();
+        var browserWindow = this.browserWidth();
+        var containerWidth = this.contentWidth();
+        this.placement(browserWindow, containerWidth);
+        $(window).on('resize', this.OnResize);
+        $(window).scroll(this.OnScroll);
+    },
+    browserWidth: function () {
+        return $(document).width();
+    },
+    browserHeight: function () {
+        return $(document).height();
+    },
+    contentWidth: function () {
+        return $('.container-small').width();
+    }, contentHeight: function () {
+        return $('.container-small').height();
+    },
+    placement: function (browserWindow, containerWidth) {
+        var ShareBlockPosition = (browserWindow - containerWidth) / 2 - this.config.offset;
+        $('.share-block').css('left', ShareBlockPosition)
+    },
+    show: function () {
+        $('.share-block').fadeIn();
+    },
+    hide: function () {
+        $('.share-block').fadeOut();
+    },
+    OnResize: function () {
+        var window = $(this); //this = window
+        if (win.width() >= 1280) {
+            shareBlock.show();
+        } else {
+            shareBlock.hide();
+        }
+    },
+    OnScroll: function () {
+        var heightScrolled = $(window).scrollTop();
+        var shareBlockPositionActive = (heightScrolled) / shareBlock.contentHeight() * 100;
+        //By default, appear when > than 20%
+        if (shareBlockPositionActive >= shareBlock.config.appearWhenScrolledTo.start && shareBlockPositionActive <= shareBlock.config.appearWhenScrolledTo.stop) {
+            shareBlock.show();
+        } else {
+            shareBlock.hide();
+        }
+    }
+}
