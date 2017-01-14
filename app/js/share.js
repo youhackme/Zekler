79d76f6afee6ca5f488097885caa78302e3cb2ab
Hyder Bangash <hyder.bangash@stellatelecom.com>
2017-01-14 17:28:26 +0400

    jQuery style

--- app/js/share.js
+++ app/js/share.js
@@ -6,49 +6,51 @@ var shareBlock = {
             stop: 93
         }
     },
-    init: function (config) {
-        $.extend(this.config, config);
+    init: function( config ) {
+        $.extend( this.config, config );
         this.hide();
         var browserWindow = this.browserWidth();
         var containerWidth = this.contentWidth();
-        this.placement(browserWindow, containerWidth);
-        $(window).on('resize', this.OnResize);
-        $(window).scroll(this.OnScroll);
+        this.placement( browserWindow, containerWidth );
+        $( window ).on( "resize", this.OnResize );
+        $( window ).scroll( this.OnScroll );
     },
-    browserWidth: function () {
-        return $(document).width();
+    browserWidth: function() {
+        return $( document ).width();
     },
-    browserHeight: function () {
-        return $(document).height();
+    browserHeight: function() {
+        return $( document ).height();
     },
-    contentWidth: function () {
-        return $('.container-small').width();
-    }, contentHeight: function () {
-        return $('.container-small').height();
+    contentWidth: function() {
+        return $( ".container-small" ).width();
+    }, contentHeight: function() {
+        return $( ".container-small" ).height();
     },
-    placement: function (browserWindow, containerWidth) {
-        var ShareBlockPosition = (browserWindow - containerWidth) / 2 - this.config.offset;
-        $('.share-block').css('left', ShareBlockPosition);
+    placement: function( browserWindow, containerWidth ) {
+        var ShareBlockPosition = ( browserWindow - containerWidth ) / 2 - this.config.offset;
+        $( ".share-block" ).css( "left", ShareBlockPosition );
     },
-    show: function () {
-        $('.share-block').fadeIn();
+    show: function() {
+        $( ".share-block" ).fadeIn();
     },
-    hide: function () {
-        $('.share-block').fadeOut();
+    hide: function() {
+        $( ".share-block" ).fadeOut();
     },
-    OnResize: function () {
-        var window = $(this); //this = window
-        if (window.width() >= 1280) {
+    OnResize: function() {
+        var window = $( this ); //this = window
+        if ( window.width() >= 1280 ) {
             shareBlock.show();
         } else {
             shareBlock.hide();
         }
     },
-    OnScroll: function () {
-        var heightScrolled = $(window).scrollTop();
-        var shareBlockPositionActive = (heightScrolled) / shareBlock.contentHeight() * 100;
+    OnScroll: function() {
+        var heightScrolled = $( window ).scrollTop();
+        var shareBlockPositionActive = ( heightScrolled ) / shareBlock.contentHeight() * 100;
+
         //By default, appear when > than 20%
-        if (shareBlockPositionActive >= shareBlock.config.appearWhenScrolledTo.start && shareBlockPositionActive <= shareBlock.config.appearWhenScrolledTo.stop) {
+        if ( shareBlockPositionActive >= shareBlock.config.appearWhenScrolledTo.start &&
+             shareBlockPositionActive <= shareBlock.config.appearWhenScrolledTo.stop ) {
             shareBlock.show();
         } else {
             shareBlock.hide();
